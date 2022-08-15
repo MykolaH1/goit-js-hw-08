7efd63828e9b537ed68ea2ddd53ccf57cc088ee6
MykolaH1 <mh22051993@gmail.com>
2022-08-15 16:07:36 +0300

    2 завдання

---
+++ src/js/02-video.js
@@ -0,0 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+//записує в localStorage час
+player.on('timeupdate', throttle(function(data) {
+   localStorage.setItem ("videoplayer-current-time", JSON.stringify(data.seconds));
+},1000));
+
+
+const saveTime = localStorage.getItem("videoplayer-current-time");
+//перевіряю чи в змінній є якісь дан
+
+if(saveTime) {
+   player.setCurrentTime(JSON.parse(saveTime));
+}
